be33a7b203e6a329b6868d92f6dc720de72f6def
Akhilraj N S <akhilrajns@gmail.com>
2017-07-16 21:26:46 +0530

    settings

--- bankapp/bankapp/settings.py
+++ bankapp/bankapp/settings.py
@@ -138,3 +138,6 @@ else:
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = '/root/emaps/bankapp/bankapp/static/'
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/root/emaps/bankapp/bankapp/media/'
+MEDIA_URL = '/media/'
